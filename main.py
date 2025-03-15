957906e970b1ee362472e137acb3fd0466c37dc8
VictorLopez09 <victor.lopez68@unach.mx>
2025-03-14 19:10:50 -0600

    Se agrego un endpoint

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def index():
     return {"message": "¡Hola, mundo!"}
+
+@app.get("/libros/{id}")
+def obtener_libro(id: int):
+    return {"id": id}
